10532eae76ec64e4a31885e6ae2094a190d1065a
fixnetcodes <deogratiasamas@gmail.com>
2023-08-24 15:42:41 +0300

    added navigation menu links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,18 +6,41 @@ function Navbar() {
     const [click, setClick] = useState(false)
     
     const handleMenuClick = () => setClick(!click)
+    const closeMobileMenu = () => setClick(false)
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-            <Link to="/" className='navbar-logo'>
-                Fixnet Web <i class='fab fa-typo3' />
-            </Link>
-            <div className="menu-icon" onClick={handleMenuClick}>
-                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className='navbar-logo'>
+                    Fixnet Web <i class='fab fa-typo3' />
+                </Link>
+                <div className="menu-icon" onClick={handleMenuClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                            Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                            Products
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                            Sign Up
+                        </Link>
+                    </li>
+                </ul>
             </div>
-        </div>
-      </nav>
+        </nav>
     </>
   )
 }
