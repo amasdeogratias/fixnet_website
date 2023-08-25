7da389916d547277599aee465a7f8dcfc9e3f3ad
fixnetcodes <deogratiasamas@gmail.com>
2023-08-25 16:38:23 +0300

    implement close menu when logo is clicked, and hide signup btn on refresh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Navbar.css'
 import {Link} from 'react-router-dom';
 import Button from './Button'
@@ -17,13 +17,16 @@ function Navbar() {
           setButton(true);
         }
       };
+      useEffect(() => {
+        showButton();
+      }, []);
       
       window.addEventListener('resize', showButton);
   return (
     <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to="/" className='navbar-logo'>
+                <Link to="/" className='navbar-logo' onClick={closeMobileMenu}>
                     Fixnet Web <i class='fab fa-typo3' />
                 </Link>
                 <div className="menu-icon" onClick={handleMenuClick}>
