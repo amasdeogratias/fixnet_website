8240e4bc44463dfa2ae59507adec4b41aa197af2
fixnetcodes <deogratiasprog@gmail.com>
2023-09-08 11:32:43 +0300

    displayed all cards in the home page and their style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import '../../App.css';
 import Body from '../Body'
+import Cards from '../Cards'
 
 function Home() {
   return (
     <>
       <Body/>
+      <Cards />
     </>
   )
 }
