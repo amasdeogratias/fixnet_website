4f0a6e681787c4612184619c1be0de288a5aa575
fixnetcodes <deogratiasprog@gmail.com>
2023-09-20 10:56:01 +0300

    added footer

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import '../../App.css';
 import Body from '../Body'
 import Cards from '../Cards'
+import Footer from '../Footer'
 
 function Home() {
   return (
     <>
       <Body/>
       <Cards />
+      <Footer />
     </>
   )
 }
